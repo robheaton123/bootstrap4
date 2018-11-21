356f1c4a128821bedc81d0aa52152bcaaef20657
Robson <robson.rocha9@etec.sp.gov.br>
2018-11-21 21:52:57 -0200

    Fim do Curso Bootstrap4

--- OnePage/js/main.js
+++ OnePage/js/main.js
@@ -1,3 +1,4 @@
+/* tudo da classe .contar recebe .counterUp(conf adicionais) // Contador da seção Cursos*/
 (function($){
  	"use strict";
  	/* Inicializar nosso contador */
@@ -5,7 +6,15 @@
 		time: 1000,
 		delay: 10
 	});
+
+/*500 tempo /// animacao ao clicar no link do menu principal ira para o id selecionado no proprio site*/
+var $doc = $('html,body');
+$('.scroll-page').click(function(){
+	$doc.animate({					
+		scrollTop: $($.attr(this,'href')).offset().top
+	},500);
+	return false;
+});
+
+
 })(jQuery);
-
-
-/* tudo da classe .contar recebe .counterUp(conf adicionais) */
